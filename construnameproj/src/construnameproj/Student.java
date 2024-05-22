0e36505dab6bdb36197e34ab18476d0cc50b66d5
Jahanvi-Shantibhai-Dusara <jahanvidusara@gmail.com>
2024-05-22 12:01:07 +0530

    Add files via upload

---
+++ construnameproj/src/construnameproj/Student.java
@@ -0,0 +1,34 @@
+package construnameproj;
+
+public class Student {
+
+	private String name;
+
+	
+	public Student() {
+		
+		this.name = "Unknown";
+	}
+	
+	public Student(String name) {
+		
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + "]";
+	}
+	
+	
+	
+	
+}
---
+++ construnameproj/src/construnameproj/StudentDemo.java
@@ -0,0 +1,17 @@
+package construnameproj;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		
+		Student s = new Student();
+		System.out.println("Student name 1: "+s);
+		
+		Student s1 = new Student("Jahanvi");
+		System.out.println("Student name 2: "+s1);
+		
+		
+
+	}
+
+}
