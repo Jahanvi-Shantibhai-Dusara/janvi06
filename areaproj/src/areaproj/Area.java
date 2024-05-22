2f08d00221e39bf85c0cb33d161b70410aac2567
Jahanvi-Shantibhai-Dusara <jahanvidusara@gmail.com>
2024-05-22 11:09:34 +0530

    Add files via upload

---
+++ areaproj/src/areaproj/Area.java
@@ -0,0 +1,18 @@
+package areaproj;
+
+public class Area {
+	
+		private double length,breadth;
+
+		public void setDim(double length,double breadth)
+		{
+			this.length = length;
+			this.breadth = breadth;
+		}
+		
+		public double getArea()
+		{
+			return length * breadth;
+		}
+		
+}
---
+++ areaproj/src/areaproj/Areademo.java
@@ -0,0 +1,15 @@
+package areaproj;
+
+public class Areademo {
+
+	public static void main(String[] args) {
+		
+		Area a = new Area();
+		
+		a.setDim(5.0,3.0);
+		
+		System.out.println("Area of rectangle is: "+a.getArea());
+		
+	}
+
+}
