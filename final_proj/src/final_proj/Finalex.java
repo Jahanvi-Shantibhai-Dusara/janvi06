c5d9e74373d338f56e2766a898725b2aaf8dcaec
Jahanvi-Shantibhai-Dusara <jahanvidusara@gmail.com>
2024-06-05 17:53:29 +0530

    Add files via upload

---
+++ final_proj/src/final_proj/Finalex.java
@@ -0,0 +1,12 @@
+package final_proj;
+
+public class Finalex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Circle c = new Circle();
+		c.areaofcircle();
+	}
+
+}
