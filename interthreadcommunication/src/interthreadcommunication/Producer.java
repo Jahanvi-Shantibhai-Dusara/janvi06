f2612d44c176d7a499cecc979d8f348264f77ffc
Jahanvi-Shantibhai-Dusara <jahanvidusara@gmail.com>
2024-06-18 14:03:46 +0530

    Add files via upload

---
+++ interthreadcommunication/src/interthreadcommunication/Producer.java
@@ -0,0 +1,38 @@
+package interthreadcommunication;
+
+public class Producer implements Runnable {
+
+	Q q;
+
+	
+	public Producer(Q q) 
+	{
+	
+	this.q = q;
+	
+	Thread t = new Thread(this,"Producer");
+	t.start();
+    }
+	
+	@Override
+	public void run() {
+	int i=0;	
+	while(true) 
+	{
+		q.setNum(i++);
+		try 
+		{
+			Thread.sleep(2000);
+		} 
+		catch (InterruptedException e) 
+		{
+			
+			e.printStackTrace();
+		}
+	}
+	
+	}
+
+
+	
+}
---
+++ interthreadcommunication/src/interthreadcommunication/Q.java
@@ -0,0 +1,44 @@
+package interthreadcommunication;
+
+public class Q {
+
+	int num;
+	 boolean valueSet = false;
+		public  synchronized void getNum() {
+			
+			while(!valueSet) 
+			{
+				try 
+				{
+					wait();
+				} 
+				catch (InterruptedException e) 
+				{
+					e.printStackTrace();
+				}
+			}
+			System.out.println("get = " + num);
+			valueSet=false;
+			notify();
+		}
+		
+		public synchronized void setNum(int num) {
+			while(valueSet) 
+			{
+				try 
+				{
+					wait();
+				} 
+				catch (InterruptedException e) 
+				{
+					e.printStackTrace();
+				}
+			}
+			System.out.println("set = " + num);
+			this.num = num;
+			valueSet=true;
+			notify();
+		}
+
+
+}
